f1680877cfc626a7ec7abd4b7ac4a8296a335580
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-12 13:14:57 -0500

    implemented bubble sort method

--- algorithmic_strategies_exercise/03_bubble_sort.rb
+++ algorithmic_strategies_exercise/03_bubble_sort.rb
@@ -3,6 +3,24 @@
 # The method should return the array.
 # Do not use the built-in Array#sort
 
+def bubble_sort(arr)
+
+  for j in 0..arr.length - 1  
+    
+    for i in 0..arr.length - 2
+      c = i + 1
+      case arr[i] <=> arr[c]
+      when 1
+        arr[i], arr[c] = arr[c], arr[i]
+      else 
+        next
+      end
+    end
+
+  end
+
+  arr
+end
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
