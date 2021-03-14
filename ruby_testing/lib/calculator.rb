5f8a7e777a89612f91ba88caea72ff935220380d
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-14 13:19:24 -0400

    implemented multiply method and test

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def add(a,*b)
     a + b.sum
   end
+
+  def multiply(a, *b)
+    a * b.reduce{|acc, num| acc*num}
+  end
+
+  def subtract
+
+  end
+
+  def divide
+
+  end
 end
--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -12,4 +12,16 @@ describe Calculator do
       expect(calculator.add(2, 5, 7)).to eql(14)
     end
   end
+
+  describe "#multiply" do
+    it "returns the product of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 2)).to eql(10)
+    end
+
+    it "returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(2, 5, 7)).to eql(70)
+    end
+  end
 end
