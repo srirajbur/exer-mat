958c4cd5e2e8e5ddc83dbdb09d48c85999de18a4
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-16 00:52:42 -0400

    implemented alternating_case method

--- rspec_exercise_1/lib/part_1.rb
+++ rspec_exercise_1/lib/part_1.rb
@@ -13,3 +13,17 @@ end
 def yell(str)
   str.upcase + "!"
 end
+
+def alternating_case(sentence)
+  arr = sentence.split(" ")
+  new = []
+  arr.each_with_index do |ele, idx|
+    if idx == 0 || idx%2==0
+      new << ele.upcase
+    else
+      new << ele.downcase
+    end
+  end
+
+  new.join(" ")
+end
