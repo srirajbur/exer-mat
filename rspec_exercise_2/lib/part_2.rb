1d08992fe068ab0970b31de00dd1a582e9cc03be
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-16 23:48:43 -0400

    implemented sub_strings method

--- rspec_exercise_2/lib/part_2.rb
+++ rspec_exercise_2/lib/part_2.rb
@@ -8,3 +8,17 @@ def palindrome?(string)
 
   string == new
 end
+
+def substrings(string)
+  i = 0
+  sub_strings = []
+  while i < string.length
+    new = ""
+    (i...string.length).each do |idx|
+      new += string[idx]
+      sub_strings << new
+    end
+    i += 1
+  end
+  sub_strings
+end
