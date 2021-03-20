a7f636d64759296566cd978b2040e6c15e89c3c0
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-20 03:04:24 -0400

    implemented largest_prime_factor method

--- debugging_exercise_2/lib/problems.rb
+++ debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,17 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+def prime?(num)
+  return false if num < 2
+
+  (2...num).none? { |i| num % i == 0}
+end
+
+def largest_prime_factor(num)
+  num.downto(2) do |factor|
+    if num % factor == 0 && prime?(factor)
+      return factor
+    end 
+  end
+end
