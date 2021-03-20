a3918a3283446e18b9c96e962daccb65fd472868
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-20 03:30:15 -0400

    implemented dupe_indices method

--- debugging_exercise_2/lib/problems.rb
+++ debugging_exercise_2/lib/problems.rb
@@ -1,7 +1,7 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
-
+require "byebug"
 def prime?(num)
   return false if num < 2
 
@@ -26,3 +26,24 @@ def unique_chars?(string)
   end 
   true
 end
+
+def dupe_indices(array)
+  hash = Hash.new{|h,k| h[k] = [] } # hash["key"] << []
+
+  array.each_with_index do |ele, idx|
+    if array.count(ele) > 1
+      hash[ele].push(idx)
+    else
+      next
+    end
+  end
+  
+  hash
+
+end
+
+# it "should return an hash where keys are the elements that were repeated in the array and values are the indices where that element appears" do
+#   expect(dupe_indices(["a", "b", "c", "c", "b"])).to eq({"b"=>[1,4], "c"=>[2,3]})
+#   expect(dupe_indices(["a", "a", "c", "c", "b", "c"])).to eq({"a"=>[0,1], "c"=>[2,3,5]})
+#   expect(dupe_indices(["a", "b", "c"])).to eq({})
+# end
