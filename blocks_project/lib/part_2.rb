1ae2a4b59a8ab7ffe0d6bcffd6dadf6933871f60
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-21 22:27:49 -0400

    implemented no_valid_url? method

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -3,3 +3,11 @@
 def all_words_capitalized?(words)
   words.all? { |word| word == word.capitalize }
 end
+
+def no_valid_url?(urls)
+  domain_end = ['.com', '.net', '.io', '.org']
+  urls.none? do |url|  
+    domain_end.any? { |ending| url.end_with?(ending) }
+
+  end
+end
