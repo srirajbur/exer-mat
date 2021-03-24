2ba516ad1018458397bb59a874bf9578ec8a2a0c
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-23 23:01:22 -0400

    implemented instance methods for Dog class

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,41 @@
 class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end 
 
+  def name
+    @name
+  end
+
+  def breed 
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def bark
+    if self.age > 3
+      return @bark.upcase
+    else
+      return @bark.downcase
+    end
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  def favorite_food?(string)
+    @favorite_foods.map(&:downcase).include?(string.downcase)
+  end
 end
