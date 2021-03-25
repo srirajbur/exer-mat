0245b48c32bb3bd8847cc855568453f768a39eff
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-24 22:46:07 -0400

    implemented average instance method in Array Class

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,7 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def average
+    return nil if self.length == 0
+    self.sum / (self.length * 1.0)
+  end
 end
