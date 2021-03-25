0e38ee979a287237bcf7ff76298e2a3bb42a5bed
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-24 23:49:15 -0400

    implemented counts instance method to Array Class

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,7 +1,30 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
+# require 'byebug'
+
 class Array
   def average
     return nil if self.length == 0
     self.sum / (self.length * 1.0)
+  end
+
+  def median 
+    return nil if self.length == 0 
+    case self.length % 2
+    when 1
+      sorted = self.sort
+      middle_index = sorted.length / 2
+      sorted[middle_index]
+    when 0
+      sorted = self.sort
+      middle_index = sorted.length / 2
+      sum = sorted[middle_index] + sorted[middle_index - 1]
+      sum / (2.0)
+    end
+  end
+
+  def counts
+    count = Hash.new(0)
+    self.each { |ele| count[ele] += 1 }
+    count
   end
 end
