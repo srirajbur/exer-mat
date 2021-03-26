f9cfbe5bfa9b64861daea41be900a78f84bddad2
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-26 10:01:46 -0400

    implemented main methods for Hangman Class

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -11,4 +11,38 @@ class Hangman
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
+
+  def guess_word
+    @guess_word
+  end 
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    indices = []
+    @secret_word.split('').each_with_index do |ele, idx|
+      if char == ele
+        indices << idx
+      end
+    end
+
+    indices
+  end
+
+  def fill_indices(char, indices)
+    indices.each do |index|
+      @guess_word[index] = char
+    end
+  end
+  
 end
