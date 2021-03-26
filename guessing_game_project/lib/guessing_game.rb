55cf2b839ae51016b4bf4f333f7ffe825e24a1e0
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-26 03:53:18 -0400

    implemented needed instance methods

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -4,4 +4,31 @@ class GuessingGame
     @num_attempts = 0
     @game_over = false
   end
+
+  def num_attempts
+    @num_attempts
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(guess)
+    @num_attempts += 1
+    if guess == @secret_num
+      @game_over = true
+      puts "you win"
+    elsif guess > @secret_num
+      puts "too big"
+    else
+      puts "too small"
+    end
+  end
+
+  def ask_user
+    puts "enter a number"
+    input = gets.chomp.to_i
+    check_num(input)
+  end
+
 end
