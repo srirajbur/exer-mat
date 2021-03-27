49412e0cdfe6ed5e05888ea07a406b64389d71f0
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-27 05:37:33 -0400

    implemented necessary instance methods for Room Class

--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -3,4 +3,30 @@ class Room
     @capacity = capacity
     @occupants = []
   end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    @capacity > @occupants.length ? false : true
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    if self.full?
+      return false
+    else
+      @occupants << name
+      return true
+    end
+  end
+
 end
