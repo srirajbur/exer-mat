e1f3425f116173a0d770a3e4d35ae4967fa184d2
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-29 15:34:01 -0400

    initialized Code class

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -10,4 +10,12 @@ class Code
     chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
   end
 
+  def initialize(chars)
+    if Code.valid_pegs?(chars)
+      @pegs = chars.map(&:upcase)
+    else
+      raise "pegs are invalid"
+    end
+  end
+
 end
