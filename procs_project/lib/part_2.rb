6bea4ef2e64184fbc3bd19abc132f408db1c54cc
Sriraj Bura <sriraj.bura16@gmail.com>
2021-04-11 17:05:44 -0400

    implemented word_changer method

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -1,4 +1,14 @@
 def reverser(str, &block)
   reversed = block.call(str.reverse)
+  
   reversed
+end
+
+def word_changer(sentence, &block)
+  arr = sentence.split(' ')
+  arr.map! do |word|
+    block.call(word)
+  end
+
+  arr.join(' ')
 end
