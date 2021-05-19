720508c79275e263ce753b02398986fda9a66322
Sriraj Bura <sriraj.bura16@gmail.com>
2021-05-19 11:11:43 -0400

    add api-fetch-demo and react app

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -81,8 +81,14 @@ class Hash
     # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
-    def my_select(&prc)
-
+    def my_select(&prc = nothing)
+      
+      hash = Hash.new
+      self.each do |k, v|
+        hash[k] = v if prc.call(k, v)
+        
+      end
+      hash
     end
 end
 
