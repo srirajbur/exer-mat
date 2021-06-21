f21b7fb7fc1063a88e30f669e151adffd05954bc
Sriraj Bura <sriraj.bura16@gmail.com>
2021-06-20 20:27:39 -0400

    added function and styles

--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const { randomQuote } = require('quotegarden');
+
+  const [quote, setQuote] = useState({
+    data: [
+      {
+        _id: '5eb17ab3b69dc744b4e81942',
+        quoteText: 'Does this trolley go to Tahiti?',
+        quoteAuthor: 'Arthur Morgan',
+      },
+    ],
+  });
+
+  async function fetchData() {
+    const newQuote = await randomQuote();
+    setQuote(newQuote);
+  }
+
+  const quoteText = quote.data[0].quoteText;
+  const quoteAuthor = quote.data[0].quoteAuthor;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-generator">
+        <div className="quote-info">
+          <div className="quote">
+            <div className="quote-text">{`"${quoteText}"`}</div>
+            <div className="quote-author">{quoteAuthor}</div>
+            <button className="random-quote-btn" onClick={fetchData}>
+              Random Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
