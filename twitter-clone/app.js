831d8101c58c0cb693a61a074bf8d2758949d41b
Sriraj Bura <sriraj.bura16@gmail.com>
2021-10-29 18:13:42 -0400

    implemented routes and db

--- twitter-clone/app.js
+++ twitter-clone/app.js
@@ -3,6 +3,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+//Config
+require('./config/db');
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Router
+const users = require('./routes/api/users');
+// const tweets = require('./routes/api/tweets');
+
+app.use('/api/users', users);
+// app.use('/api/tweets', tweets);
+
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(port, () => {
---
+++ twitter-clone/config/db/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const mongoDB = process.env.DB_HOST;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = db;
