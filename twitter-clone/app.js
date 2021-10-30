e29a809df86c2955b71c48c497746cf9f5df8384
Sriraj Bura <sriraj.bura16@gmail.com>
2021-10-30 17:04:44 -0400

    implemented user model

--- twitter-clone/app.js
+++ twitter-clone/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
 
 //Config
+require('dotenv').config();
 require('./config/db');
 
 //Middleware
