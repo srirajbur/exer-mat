321246781b80c546ed1b27a0619c46e709a0066b
Sriraj Bura <sriraj.bura16@gmail.com>
2021-11-02 05:24:43 -0400

    added header for auth

--- twitter-clone/routes/api/users.js
+++ twitter-clone/routes/api/users.js
@@ -82,7 +82,7 @@ router.post('/login', (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: 'Bearer' + token,
+              token: 'Bearer ' + token,
             });
           }
         );
